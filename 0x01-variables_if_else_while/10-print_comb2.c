16fbb3cfb25afcd2480bb59f3c92908c8f2aa9ac
poix-code <1996@holbertonschool.com>
2020-06-11 22:04:50 -0500

    Variables, if, else, while

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,11 +6,20 @@
  */
 int main(void)
 {
-	int num;
+	int num1, num2;
 
-	for (num = 0; num <= 9; num++)
+	for (num2 = 0; num2 <= 9; num2++)
 	{
-		putchar(num + 48);
+		for (num1 = 0; num1 <= 9; num1++)
+		{
+		putchar(num2 + 48);
+		putchar(num1 + 48);
+		if (num2 <= 9 && num1 <= 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
 	}
 	putchar('\n');
 	return (0);
