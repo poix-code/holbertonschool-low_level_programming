cadcd7ef5cb41f85441d6a3676cf6f40ccb912b1
poix-code <1996@holbertonschool.com>
2020-06-23 13:47:44 -0500

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * rev_string - The funcion rev a string.
+ * @s: The variable contains the string.
+ * Return: Void.
+ */
+void rev_string(char *s)
+{
+	char temp, len;
+	int i, k;
+
+	len = _strlen(s) -1;
+	k = len;
+	for(i = 0; i < k; i++)
+	{
+		temp = s[k];
+		s[k] = s[i];
+		s[i] = temp;
+		k--;
+	}
+}
+
+/**
+ * _strlen - The function returns the length of a string.
+ * @s: Variable of the string.
+ * Return: integer value.
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+		;
+	return (length);
+}
