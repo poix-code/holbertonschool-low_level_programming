0b045c9025d772122770b5292cfce28cf9b5d1d6
poix-code <1996@holbertonschool.com>
2020-06-25 20:30:22 -0500

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - Concatenates two strings.
+ * @dest: The variable had within one string.
+ * @src: Contains within other string.
+ * Return: dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i1, i2;
+
+	i1 = 0;
+	i2 = 0;
+	while (dest[i1])
+		dest++;
+	while (src[i2])
+	{
+		dest[i1 + i2 + 1] = src[i2];
+		i2++;
+	}
+	return (dest);
+}
