8034cba880dd6936590c4f701c5823797b0571c1
poix-code <1996@holbertonschool.com>
2020-06-29 18:04:06 -0500

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,19 +1,22 @@
 #include "holberton.h"
 
 /**
- * *_strchr - THe function locates a character in a string.
+ * *_strchr - The function locates a character in a string.
  * @s: The string to be evaluated.
  * @c: Character to be evaluated.
  * Return: A pointer to first character found or NULL if it isn't.
  */
 char *_strchr(char *s, char c)
 {
-	char *preturn;
-
-	for (preturn = s; *preturn; preturn++)
+	while (*s)
 	{
-		if (*preturn == c)
-			return (preturn);
+		if (*s == c)
+			return (s);
+		s++;
 	}
-	return ('\0');
+	if (*s == '\0')
+	{
+		return ('\0');
+	}
+	return (s);
 }
