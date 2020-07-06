430c8550c71ea005e9a2b4873280a95c009d8061
poix-code <1996@holbertonschool.com>
2020-07-05 23:27:41 -0500

    Argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,24 +9,20 @@
  */
 int main(int argc, char **argv)
 {
-	int i, mul, ret;
+	int mul, ret;
 
-	i = 0;
-	mul = 1;
+	mul = 0;
 	ret = 0;
-	if (argc > 1 && argc < 3)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			mul *= atoi(argv[i]);
-		}
-		printf("%d\n", mul);
-		ret = 0;
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		ret = 1;
+	}
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+		ret = 0;
 	}
 	return (ret);
 }
