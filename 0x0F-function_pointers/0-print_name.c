99a7215f85d30a9fcf9020cfe2c45de3dfee08a6
poix-code <1996@holbertonschool.com>
2020-07-15 18:54:08 -0500

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - The function prints a name.
+ * @name: string to print.
+ * @f: pointer to function.
+ *
+ * Return:  void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,7 @@
+# Project: 0x0F.C-Function pointers
+
+## 0-print_name.c
+
+The function prints a name.
+
+## 
