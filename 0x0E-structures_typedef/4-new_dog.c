2b4769002ecae21d493dd3fbbad125767a4d82d1
poix-code <1996@holbertonschool.com>
2020-07-14 21:22:01 -0500

    Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,9 +14,70 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dg = malloc(sizeof(dog_t));
 	if (new_dg == NULL)
+	{
+		free(new_dg);
 		return (NULL);
-	new_dg->name = name;
+	}
+	if (name)
+	{
+		new_dg->name = malloc(sizeof(char) * (_strlen(name) + 1));
+		if (new_dg->name == NULL)
+		{
+			free(new_dg);
+			return (NULL);
+		}
+		_strcpy(new_dg->name, name);
+	}
+	else
+		new_dg->name = NULL;
+	if (owner)
+	{
+		new_dg->owner = malloc(sizeof(char) * (_strlen(name) + 1));
+		if (new_dg->owner == NULL)
+		{
+			free(new_dg->name);
+			free(new_dg->owner);
+			free(new_dg);
+			return (NULL);
+		}
+		_strcpy(new_dg->owner, owner);
+	}
+	else
+		new_dg->owner = NULL;
 	new_dg->age = age;
-	new_dg->owner = owner;
 	return (new_dg);
+}
+
+/**
+ * _strlen - The function returns the length of a string.
+ * @s: Variable of the string.
+ * Return: integer value.
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; *s; length++)
+	{
+		s++;
+	}
+	return (length);
+}
+
+/**
+ * *_strcpy - Redirections with strings.
+ * @dest: the variable contains s1.
+ * @src: The variable contains the string.
+ * Return: dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int c, length;
+
+	length = _strlen(src) + 1;
+	for (c = 0; c < length && src[c] != '\0'; c++)
+		dest[c] = src[c];
+	if (c < length)
+		dest[c] = '\0';
+	return (dest);
 }
