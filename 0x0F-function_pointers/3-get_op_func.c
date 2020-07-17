3096e030641c60e8da4213d26804a90053da956f
poix-code <1996@holbertonschool.com>
2020-07-17 11:42:13 -0500

    Function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,10 +18,12 @@ int (*get_op_func(char *s))(int, int)
 };
 	int i;
 
-	for (i = 0; ops[i].op; i++)
+	i = 0;
+	while (ops[i].op)
 	{
 		if (!strcmp(ops[i].op, s))
 			return (ops[i].f);
+		i++;
 	}
 	printf("Error\n");
 	exit(99);
