0984f0313b66b5511cde589efc8493b799df764d
poix-code <1996@holbertonschool.com>
2020-07-16 22:09:44 -0500

    Function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,7 +20,7 @@ int (*get_op_func(char *s))(int, int)
 };
 	int i;
 
-	for (i = 0; ops[i].op; i++)
+	for (i = 0; ops[i].op != '\0'; i++)
 	{
 		if (ops[i].op[0] == s[0] && s[1] == '\0')
 			return (ops[i].f);
