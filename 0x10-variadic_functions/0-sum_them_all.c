f5c3413909b6ef79d6bc55c6b5c77adcca4281b1
poix-code <1996@holbertonschool.com>
2020-07-17 20:43:58 -0500

    Variadic, sum all of its parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - The function sum all its parameters.
+ * @n: Define the amount of the arguments.
+ * Return: The sum's result.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	int sum = 0;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+	va_end(list);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,5 @@
+# Project: 0x10.C-Variadic functions
+
+## 0-sum_them_all.c
+
+The function returns the sum of all its parameters.
