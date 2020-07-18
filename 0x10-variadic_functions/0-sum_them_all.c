4afa540a8f5515c952ea5689e88bb697847f12f4
poix-code <1996@holbertonschool.com>
2020-07-17 20:57:59 -0500

    Variadic, sum all of its parameters(add the if statement)

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,9 +11,14 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
-	va_start(list, n);
-	for (i = 0; i < n; i++)
-		sum += va_arg(list, int);
-	va_end(list);
+	if (n == 0)
+		sum = 0;
+	else
+	{
+		va_start(list, n);
+		for (i = 0; i < n; i++)
+			sum += va_arg(list, int);
+		va_end(list);
+	}
 	return (sum);
 }
