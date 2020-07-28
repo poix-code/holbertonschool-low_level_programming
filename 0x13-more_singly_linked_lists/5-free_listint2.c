611e6536e6585dd2689531f2817659d81808057d
poix-code <1996@holbertonschool.com>
2020-07-28 18:07:26 -0500

    Check the correct syntax and I correct the conditions of the code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *freeNode;
 
+	if (head == NULL)
+		return (0);
 	while (*head)
 	{
 		freeNode = *head;
-		*head =(*head)->next;
+		*head = (*head)->next;
 		free(freeNode);
 	}
 	*head = NULL;
