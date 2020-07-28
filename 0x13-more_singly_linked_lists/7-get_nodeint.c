74c533ba5b2c91f92abe5b6f8635f5c37578fc90
poix-code <1996@holbertonschool.com>
2020-07-28 18:22:21 -0500

    Check the logic of the conditions of 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,8 +11,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *getIndex;
 
-	if (!(head))
-		return (0);
 	getIndex = head;
 	for (i = 0; i < index; i++)
 	{
