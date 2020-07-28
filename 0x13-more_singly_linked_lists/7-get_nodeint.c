85e1c1c016019fd9131b5a78e92064df2a21a560
poix-code <1996@holbertonschool.com>
2020-07-28 18:31:39 -0500

    Return the correct value

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *getIndex;
 
+	if (head == NULL)
+		return;
 	getIndex = head;
 	for (i = 0; i < index; i++)
 	{
