32e900ac9e40a5d3b5ceaf21b41dd45863d90daf
poix-code <1996@holbertonschool.com>
2020-07-28 18:01:21 -0500

    Editing the description of the function 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * add_nodeint - .
- *
- * Return: Always 0.
+ * add_nodeint - adds a new node at the beginning of the linked list.
+ * @head: Points to the pointer to the head of the linked list.
+ * @n: Contains within the data of the list.
+ * Return: The new node.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
