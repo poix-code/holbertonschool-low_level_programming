09837c4bcbc3956870f587f95fe3104514387f26
poix-code <1996@holbertonschool.com>
2020-07-30 23:11:12 -0500

    Tested in C tutor

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - show the number of bits that you need to flip other number.
+ * @n: Number to compare.
+ * @m: check the flip.
+ * Return: THe number of bits that you need to flip.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int flip = 0;
+	int SIZE = ((sizeof(unsigned long int) * 8) - 1);
+	unsigned long int cBit = n ^ m;
+
+	for (SIZE = SIZE - 1; SIZE >= 0; SIZE--)
+		if (((cBit >> SIZE) & 1) == 1)
+			flip += 1;
+	return (flip);
+}
