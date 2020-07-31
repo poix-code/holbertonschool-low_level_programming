60cbd5f37d72d0f899a30b4ca3c8a5a3650c20ad
poix-code <1996@holbertonschool.com>
2020-07-30 21:49:32 -0500

    Tested in C Tutor

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <limits.h>
+
+/**
+ * get_bit - Get the value of a bit at a given index.
+ * @n: Number to get the bit value.
+ * @index: Indicates the position.
+ * Return: The value of the bit at index, -1 if an error occured.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int res = 0;
+
+	if (index > INT_MAX)
+		return (-1);
+	res = n >> index;
+	if (res & 1)
+		return (1);
+	else
+		return (0);
+	return (0);
+}
