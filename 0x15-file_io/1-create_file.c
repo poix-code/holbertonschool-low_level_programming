133cd7f04f61aa8bd4d676403af59d13f9c44be0
poix-code <1996@holbertonschool.com>
2020-08-04 17:54:09 -0500

    Code tested in a terminal, also update the library with a function _strlen and the function to make files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	fileOpen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fileOpen == -1)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content == 0)
 		return (1);
 	fileLen = _strlen(text_content);
 	fileWrite = write(fileOpen, text_content, fileLen);
