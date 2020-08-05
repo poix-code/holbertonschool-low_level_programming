a2b30454e70534f2cae5523e0018f603f0cea3c7
poix-code <1996@holbertonschool.com>
2020-08-05 17:20:47 -0500

    Checking the loop's logic

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,8 +22,8 @@ int main(int argc, char *argv[])
 	fileOpen2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fileOpen2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	fileRead = 1024;
-	while (fileRead == 1024)
+	fileOpen1 = 1024;
+	while (fileOpen1 == 1024)
 	{
 		fileRead = read(fileOpen1, buffer, 1024);
 		if (fileRead == -1)
