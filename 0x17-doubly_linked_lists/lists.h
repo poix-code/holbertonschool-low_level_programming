1656653d9193d49ec75a4ee17ec85c74820c3f21
poix-code <1996@holbertonschool.com>
2020-09-06 21:52:40 +0000

    Finding somes errors

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -21,5 +23,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif /* __LISTS_H__ */
