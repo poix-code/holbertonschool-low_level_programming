803116a3a85840f4a926a6416858f21bbe1693d2
poix-code <1996@holbertonschool.com>
2020-09-06 22:25:27 +0000

    Function tested in python tutor before uploaded

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a double linked list.
+ * @head: points to the head of the linked list.
+ * @n: contains the data of the new node.
+ * Return: a pointer to the new node.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode;
+
+	if (!head)
+		return (0);
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = n;
+	newNode->next = *head;
+	newNode->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = newNode;
+	*head = newNode;
+	return (newNode);
+}
