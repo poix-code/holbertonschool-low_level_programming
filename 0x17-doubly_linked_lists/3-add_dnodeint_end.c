b78f5dccad276fef5b45f81c106c5330d48908f4
poix-code <1996@holbertonschool.com>
2020-09-06 23:00:31 +0000

    Change conditionals

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -20,7 +20,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	else
 	{
 		tmp = *head;
-		while (tmp->next != NULL)
+		if (tmp->next != NULL)
 			tmp = tmp->next;
 		tmp->next = newNode;
 		newNode->prev = tmp;
