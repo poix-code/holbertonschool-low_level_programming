0bd2a53bdd2552de051cfe13d114c645b51b968c
poix-code <1996@holbertonschool.com>
2020-09-07 01:47:14 +0000

    Changing the variable description

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_dlisint - returns the sum of all the data (n) of a list.
+ * sum_dlistint - returns the sum of all the data (n) of a list.
  * @head: points to the head of the linked list.
  * Return: the total of the sum.
  */
