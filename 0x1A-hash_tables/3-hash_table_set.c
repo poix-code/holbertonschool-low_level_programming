f99d3b630b27ea8dac8ff9ae1a195e92bf322e85
poix-code <1996@holbertonschool.com>
2020-10-03 02:16:08 +0000

    checking the conditionals, '\!' missing in strcmp

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node = ht->array[keyIndex];
 	while (node != NULL)
 	{
-		if (strcmp(node->key, key))
+		if (!strcmp(node->key, key))
 		{
 			free(node->value);
 			node->value = (char *)strdup(value);
