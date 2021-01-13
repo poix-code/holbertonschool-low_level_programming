f8b1ac15784be9582e549e3ca0ffb4f10dbb4ffc
poix-code <1996@holbertonschool.com>
2021-01-13 19:01:19 +0000

    Changing the 'for' condition

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,7 +13,7 @@ int linear_search(int *array, size_t size, int value)
 
 	if (!array || size == 0)
 		return (-1);
-	for (; array[i]; i++)
+	for (; i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
